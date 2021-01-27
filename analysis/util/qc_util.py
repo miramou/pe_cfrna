5131cd3a18b898691f10a7f8fd45518bab75a237
Mira Moufarrej <miramou@stanford.edu>
2021-01-27 14:31:16 -0800

    added utils to reorg ind datasets into expected schema

--- analysis/util/qc_util.py
+++ analysis/util/qc_util.py
@@ -2,10 +2,12 @@
 
 import pandas as pd
 import numpy as np
+import re
 
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler, MinMaxScaler, RobustScaler
 
+from util.gen_utils import read_sample_meta_table
 from util.plot_utils import *
 
 def filter_genes_CPMcutoff(CPM_df, CPM_cutoff = 0.5, frac_samples_exp = 0.75):
@@ -176,3 +178,96 @@ def pca_and_viz_qc(qc_data, logCPM_df, gene_qc_mask, pca_plot_title):
 	plt.suptitle(pca_plot_title, y = 1.05)
 	plt.tight_layout()
 	return heatmap, pca
+
+def read_save_delvecchio_meta(biosample_results_path, sra_results_path, out_path):
+	'''
+	Utility to read biosample_result.txt and sra_results files and pull out relevant information. 
+	Specific to the DelVecchio et al files
+
+	Input:
+		biosample_results_path - filepath to file that contains Biosample results
+		sra_results_path - filepath to file that contains SRA Run Table 
+		out_path - filepath to save metadata asso with Delvecchio et al 
+	Returns
+		pandas df with relevant sample specific metadata
+	'''
+	with open(biosample_results_path) as fp:
+		curr_line = fp.readline()
+		
+		is_sample_line = True
+		data_dict = {key : [] for key in ['subj_id', 'sample_type', 'sample_id']}
+		
+		while curr_line:
+			if is_sample_line:
+				elems = re.split(": |, |\n| \(|\)", curr_line)
+				data_dict['subj_id'].append(elems[1].replace(" ", "_"))
+				data_dict['sample_type'].append(elems[2].replace(" ", "_"))
+			if 'BioSample: SAMN' in curr_line :
+				data_dict['sample_id'].append(re.search('SAMN\\d+', curr_line).group())
+			
+			is_sample_line = True if curr_line == "\n" else False
+			curr_line = fp.readline()
+
+	meta = pd.DataFrame(data_dict).set_index('sample_id').sort_values('subj_id')
+	sra_run_table = read_sample_meta_table(sra_results_path)
+
+	meta = meta.merge(sra_run_table.loc[:, ['BioSample', 'complication_during_pregnancy']], left_index = True, right_on = 'BioSample', sort = True)
+	meta.reset_index(inplace = True)
+	meta.set_index('Run', inplace = True)
+
+	#Add term col
+	meta.insert(meta.shape[1], 'term', np.nan) 
+	for label, term in {'1st_Trimester' : 1, '2nd_Trimester' : 2, '3rd_Trimester' : 3}.items():
+		meta.loc[meta.sample_type == label, 'term'] = term
+
+	meta.insert(meta.shape[1], 'case', 0) 
+	meta.loc[meta.complication_during_pregnancy.str.contains('Preeclampsia'), 'case'] = 1
+
+	meta.index.rename('sample', inplace = True)
+	meta.to_csv(out_path)
+	return meta
+
+def read_save_munchel_data(filepath, cnts_out_path, meta_out_path, mult_samples_per_person, is_all_nt = False):
+	'''
+	Utility to read Munchel supp file and pull out relevant information. 
+	Specific to the Munchel et al 
+
+	Input:
+		filepath - filepath to counts table with metadata
+		cnts_out_path - filepath to save counts table alone [no metadata]
+		meta_out_path - filepath to save metadata alone [no cnts]
+		mult_samples_per_person - bool, True if more than 1 sample per person
+ 		is_all_nt - bool, True if all samples included in df are normotensive, False if should pull case group from sample name 
+	Returns 
+		meta - pandas df with relevant sample specific metadata
+	'''
+	df = pd.read_excel(filepath, usecols = "A,G:FB", skiprows = 3)
+	df.rename(columns = {df.columns[0] : 'gene_name'}, inplace = True)
+
+	#Pull GA metadata from 1st row
+	meta = pd.DataFrame({'ga_at_collection' : df.iloc[0, :].astype(float).round(1)}, index = df.columns).dropna()
+
+	#Remove metadata from cnts table and add to list
+	df = df.iloc[1:, :].set_index('gene_name')
+
+	#Add term, subject, and case data to meta
+	meta.insert(meta.shape[1], 'term', get_term_labels(meta, ga_col = 'ga_at_collection', has_pp = False))
+
+	subject = [subj[0] for subj in df.columns.str.split('.').to_list()] if mult_samples_per_person else df.columns
+	meta.insert(meta.shape[1], 'subject', subject)
+
+	case_grp = np.repeat('CTRL', meta.shape[0]) if is_all_nt else np.array([case_grp[0] for case_grp in df.columns.str.split('.').to_list()])
+	meta.insert(meta.shape[1], 'pe_type', case_grp)
+
+	case = np.repeat(0, meta.shape[0])
+	case[meta.loc[:, 'pe_type'].str.contains('PE')] = 1
+	meta.insert(meta.shape[1], 'case', case)
+
+	#Add dataset name
+	meta.insert(meta.shape[1], 'dataset', dataset_name)
+
+	#Save full meta and full cnts
+	meta.to_csv(meta_out_path)
+	df.to_csv(cnts_out_path)
+
+	return meta
